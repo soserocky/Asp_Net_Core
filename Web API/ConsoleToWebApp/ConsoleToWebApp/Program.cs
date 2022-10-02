a138869060a8c08aea536f88e2d8303b9996ba7f
Soserocky <sabya9106@gmail.com>
2022-10-02 15:20:23 +0530

    Pushing changes to Git

---
+++ Web API/ConsoleToWebApp/ConsoleToWebApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleToWebApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => 
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+}
---
+++ Web API/ConsoleToWebApp/ConsoleToWebApp/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleToWebApp
+{
+    internal class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello, World. This is a web application");
+                });
+            });
+        }
+
+        internal void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+    }
+}
